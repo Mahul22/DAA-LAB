e41c5a7ce25a9a45998e8cc60b1a85c539d4ddae
Mahul22 <121723582+Mahul22@users.noreply.github.com>
2024-01-15 10:39:52 +0530

    Create Assignment4.c

---
+++ Assignment4.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void bitonic(int arr[],int n){
+    for(int i=0;i<7;i++){
+        if(arr[i]>arr[i+1]){
+            printf("The bitonic number is %d.\n",arr[i]);
+            return;
+            
+            }
+        else{
+            
+            i++;
+        }
+ }
+ }
+int main(){
+    int arr[7];
+    for(int i=0;i<7;i++){
+        scanf("%d",&arr[i]);
+       }
+       
+    bitonic(arr,5);
+    return 0;
+}
